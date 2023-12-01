5c625a91243b4d54b8cc3974ecc55ddd66b32d3d
Alejandro Martín <91533700+msalexms@users.noreply.github.com>
2023-12-01 11:25:02 +0100

    Adding results and usage to README.md

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 ### Code for the project "Predicting humans presence using ESP-32"
+
+# Predicting Humans Presence with ESP-32
+
+## Overview
+
+This project utilizes an ESP-32 microcontroller to predict human presence. The machine learning model is based on the EfficientNet architecture, fine-tuned to classify two classes: `non_person` and `person`.
+
+## Model Fine-tuning
+
+The EfficientNet model was fine-tuned over 50 epochs using a training dataset of 800 images. 
+
+
+
+
+## Results
+
+### Accuracy Graph
+
+![Accuracy Graph](figures/accuracy_pretrained_True_prueba2.png)
+
+Caption: Displaying the accuracy of the model during training and validation.
+
+### Loss Function Graph
+
+![Loss Function Graph](figures/loss_pretrained_True_prueba2.png)
+
+Caption: Illustrating the variation of the loss function throughout the training process.
+
+### Confusion Matrix
+
+![Confusion Matrix](figures/confmatrix.png)
+
+Caption: Confusion matrix depicting the model's performance on the test dataset.
+
+## Data
+
+All the images used for training, validation and testing are located in the `input` folder.
+Inside is the 'test' folder, which contains the images used for testing the model. 
+The 'person_dataset' contains the two clases with the images used for training and validation.
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/curso-verano-iot-uah/efficient-net.git
+   
+2. Install the required packages:
+
+   ```bash
+   conda env create -f environment.yaml
+   ```
+3. Activate the environment:
+
+   ```bash
+   conda activate efficient-net
+   ```
+4. Run the confusion matrix script: to obtain the results of thes test dataset.
+
+   ```bash
+   python ./src/confusion_matrix.py
+   ```
---
+++ src/confusion_matrix.py
@@ -0,0 +1,84 @@
+import torch
+import cv2
+import numpy as np
+import glob as glob
+import os
+import matplotlib
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score, f1_score
+from model import build_model
+from torchvision import transforms
+
+# Constants.
+DATA_PATH = '../input/test'
+IMAGE_SIZE = 224
+DEVICE = 'cpu'
+class_names = ['non_person', 'person']
+
+# Load the trained model.
+model = build_model(pretrained=False, fine_tune=False, num_classes=2)
+checkpoint = torch.load('../weights/model_pretrained_True_prueba2.pth', map_location=DEVICE)
+model.load_state_dict(checkpoint['model_state_dict'])
+
+model.eval()
+print('Loading trained model weights...')
+
+# Lists to store ground truth and predicted labels.
+true_labels = []
+predicted_labels = []
+
+# Get all the test image paths.
+all_image_paths = glob.glob(f"{DATA_PATH}/*/*.jpg")
+
+# Iterate over all the images and do forward pass.
+for image_path in all_image_paths:
+    gt_class_name = image_path.split(os.path.sep)[-2]
+    true_labels.append(class_names.index(gt_class_name))
+
+    try:
+        image = cv2.imread(image_path)
+        if image is None:
+            print(f"Error al leer la imagen: {image_path}")
+            continue
+
+        orig_image = image.copy()
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+        ])
+        image = transform(image)
+        image = torch.unsqueeze(image, 0)
+        image = image.to(DEVICE)
+
+        outputs = model(image)
+        outputs = outputs.detach().numpy()
+        pred_class_name = class_names[np.argmax(outputs[0])]
+        predicted_labels.append(class_names.index(pred_class_name.lower()))
+        # print('GT: ' + gt_class_name + ' Pred: ' + pred_class_name)
+
+    except Exception as e:
+        print(f"Error al procesar la imagen {image_path}: {str(e)}")
+    continue
+
+conf_matrix = confusion_matrix(true_labels, predicted_labels)
+print("Confusion Matrix:")
+print(conf_matrix)
+disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=['non_person', 'person'])
+disp.plot(cmap='Greens', values_format='d')
+matplotlib.pyplot.show()
+
+precision = precision_score(true_labels, predicted_labels)
+recall = recall_score(true_labels, predicted_labels)
+f1 = f1_score(true_labels, predicted_labels)
+
+print('Precision: ' + str(precision))
+print('Recall: ' + str(recall))
+print('F1: ' + str(f1))
+
+
