23e04291f3677862c914e0ef9c92868ec8737b87
Alejandro Martín <alejandro.martinsanc@edu.uah.es>
2023-12-16 01:42:40 +0100

    Better directory paths

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,28 @@
 import torch
 import matplotlib
 import matplotlib.pyplot as plt
+import os
 
 matplotlib.style.use('ggplot')
+
+
 
 
 def save_model(epochs, model, optimizer, criterion, pretrained, name, model_name):
     """
     Function to save the trained model to disk.
     """
+    userdir = os.path.expanduser('~')
+    directory = f"{userdir}/Documents/training_results/{name}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     torch.save({
         'epoch': epochs,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
         'loss': criterion,
-    }, f"/home/ams/Documents/pesos/{model_name}_pretrained_{pretrained}_{name}.pt")
-    torch.save(model, f"/home/ams/Documents/pesos/{model_name}_model{pretrained}_{name}.pt")
+    }, f"{directory}/{model_name}_pretrained_{pretrained}_{name}.pt")
 
 
 def save_plots(train_acc, valid_acc, train_loss, valid_loss, pretrained, name, model_name):
@@ -23,6 +30,10 @@ def save_plots(train_acc, valid_acc, train_loss, valid_loss, pretrained, name, m
     Function to save the loss and accuracy plots to disk.
     """
     # accuracy plots
+    userdir = os.path.expanduser('~')
+    directory = f"{userdir}/Documents/training_results/{name}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     plt.figure(figsize=(10, 7))
     plt.plot(
         train_acc, color='green', linestyle='-',
@@ -35,7 +46,7 @@ def save_plots(train_acc, valid_acc, train_loss, valid_loss, pretrained, name, m
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
-    plt.savefig(f"../outputs/accuracy_{model_name}_pretrained_{pretrained}_{name}.png")
+    plt.savefig(f"{directory}/accuracy_{model_name}_pretrained_{pretrained}_{name}.png")
 
     # loss plots
     plt.figure(figsize=(10, 7))
@@ -50,4 +61,4 @@ def save_plots(train_acc, valid_acc, train_loss, valid_loss, pretrained, name, m
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
-    plt.savefig(f"../outputs/loss_{model_name}_pretrained_{pretrained}_{name}.png")
+    plt.savefig(f"{directory}/loss_{model_name}_pretrained_{pretrained}_{name}.png")
